3cc3127daf4c82f087551d508b3d6d59d6209d59
Женя Василинчук <marakasi88887@gmail.com>
2021-06-02 19:44:02 +0300

    Added new functions to math

--- DungeonLang/lib/modules/math.cs
+++ DungeonLang/lib/modules/math.cs
@@ -21,6 +21,60 @@ namespace DungeonLang.lib.modules
                 if (args.Length != 1) throw new RuntimeException("One arg exprected");
                 return new NumberValue(Math.Cos(args[0].AsNumber()));
             });
+            Functions.SetFunction("tan", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(Math.Tan(args[0].AsNumber()));
+            });
+            Functions.SetFunction("ctan", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(1 / Math.Tan(args[0].AsNumber()));
+            });
+            Functions.SetFunction("asin", args => {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(Math.Asin(args[0].AsNumber()));
+            });
+            Functions.SetFunction("acos", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(Math.Acos(args[0].AsNumber()));
+            });
+            Functions.SetFunction("atan", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(Math.Atan(args[0].AsNumber()));
+            });
+            Functions.SetFunction("actan", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue((Math.PI / 2) - Math.Atan(args[0].AsNumber()));
+            });
+            Functions.SetFunction("abs", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(Math.Abs(args[0].AsNumber()));
+            });
+            Functions.SetFunction("sqrt", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(Math.Sqrt(args[0].AsNumber()));
+            });
+            Functions.SetFunction("pow", args =>
+            {
+                if (args.Length != 2) throw new RuntimeException("Two arg exprected");
+                return new NumberValue(Math.Pow(args[0].AsNumber(), args[1].AsNumber()));
+            });
+            Functions.SetFunction("toDegrees", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(args[0].AsNumber() * (180 / Math.PI));
+            });
+            Functions.SetFunction("toRadians", args =>
+            {
+                if (args.Length != 1) throw new RuntimeException("One arg exprected");
+                return new NumberValue(args[0].AsNumber() * (Math.PI / 180));
+            });
         }
     }
 }
